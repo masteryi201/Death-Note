22b3b8b6bcc86ce66d5bed0a9196158987da6c1d
masteryi201 <mg3gold2014@gmail.com>
2019-09-02 17:05:22 +0700

    Update reverse_tcp.sh

--- Modules/Vuln/Framework/payloads/windows/x86/meterpreter/reverse_tcp.sh
+++ Modules/Vuln/Framework/payloads/windows/x86/meterpreter/reverse_tcp.sh
@@ -117,26 +117,26 @@ payload_banner_2
 }
 function payload_banner_1 {
 if [ "$language" = "VN" ]; then
-yes="   Có   "
-no="   Không "
+yes="     Có   "
+no="     Không "
 cat << !
 Các tùy chọn của tải trọng (windows/meterpreter/reverse_tcp):
 
-   Tên            Thiết lập hiện tại  Yêu cầu   Miêu tả
-   ----           ----------------    --------  -----------
+   Tên            Thiết lập hiện tại    Yêu cầu   Miêu tả
+   ----           ----------------      --------  -----------
    EXITFUNC       $exitfunc$no   Biện pháp tạo lối thoát (Được chấp nhận: '', seh, thread, process, none)
    LHOST          $lhost$yes    Địa chỉ lắng nghe
    LPORT          $lport$yes    Cổng lắng nghe
 
 !
 elif [ "$language" = "EN" ]; then
-yes="yes"
-no="no "
+yes="     yes"
+no="     no "
 cat << !
 Payload options (windows/meterpreter/reverse_tcp):
 
-   Name           Current Setting  Required  Description
-   ----           ---------------  --------  -----------
+   Name           Current Setting       Required  Description
+   ----           ---------------       --------  -----------
    EXITFUNC       $exitfunc$no      Exit technique (Accepted: '', seh, thread, process, none)
    LHOST          $lhost$yes      The listen address
    LPORT          $lport$yes      The listen port
