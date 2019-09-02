bcddcf7e0bc23e4aef2d674164b44919ddfccff8
masteryi201 <mg3gold2014@gmail.com>
2019-09-02 18:47:35 +0700

    Add files via upload

--- Modules/Vuln/Framework/payloads/windows/x64/meterpreter/reverse_http.sh
+++ Modules/Vuln/Framework/payloads/windows/x64/meterpreter/reverse_http.sh
@@ -4,59 +4,206 @@ function defaul_payload_options {
 	exitfunc="process"
 	luri=""
 }
+function payload_compare {
+lhost_lenght=`echo "$lhost" | awk '{print length}'`
+	if  [ "$lhost_lenght" -le 18 ]; then
+		payload1
+	else
+		payload2
+	fi
+}
 function set_payload {
-
-
-
-		if [ "$new_processing_variables" = "lhost" ]; then
-		unset lhost
-		lhost=$module_set
-		elif [ "$new_processing_variables" = "lport" ]; then
-		unset lport
-		lport="$module_set"
-		elif [ "$new_processing_variables" = "luri" ]; then
-		unset luri
-		luri="$module_set"
-		elif [ "$new_processing_variables" = "exitfunc" ]; then
-		unset exitfunc
-		exitfunc="$module_set"
+		if [[ "$new_processing_variables" = "lhost" ]] || [[ "$new_processing_variables" = "LHOST" ]] || [[ "$new_processing_variables" = "Lhost" ]]; then
+			unset lhost
+			lhost=$module_set
+		elif [[ "$new_processing_variables" = "lport" ]] || [[ "$new_processing_variables" = "LPORT" ]] || [[ "$new_processing_variables" = "Lport" ]]; then
+			unset lport
+			lport="$module_set"
+		elif [[ "$new_processing_variables" = "exitfunc" ]] || [[ "$new_processing_variables" = "EXITFUNC" ]] || [[ "$new_processing_variables" = "Exitfunc" ]]; then
+			if [[ "$module_set" = "" ]] || [[ "$module_set" = "" ]] || [[ "$module_set" = "" ]]; then
+				unset exitfunc
+				exitfunc="$module_set"
+			elif [[ "$module_set" = "seh" ]] || [[ "$module_set" = "SEH" ]] || [[ "$module_set" = "Seh" ]]; then
+				unset exitfunc
+				exitfunc="$module_set"
+			elif [[ "$module_set" = "thread" ]] || [[ "$module_set" = "THREAD" ]] || [[ "$module_set" = "Thread" ]]; then
+				unset exitfunc
+				exitfunc="$module_set"
+			elif [[ "$module_set" = "process" ]] || [[ "$module_set" = "PROCESS" ]] || [[ "$module_set" = "Process," ]]; then
+				unset exitfunc
+				exitfunc="$module_set"
+			elif [[ "$module_set" = "none" ]] || [[ "$module_set" = "NONE" ]] || [[ "$module_set" = "None" ]]; then
+				unset exitfunc
+				exitfunc="$module_set"
+			else 	failed_to_validate=" $failed_validate'$module_set' $notvalid '$new_processing_variables'"
+				echo -e "$red[-]$RESET" $failed_to_validate
+			fi
+		elif [[ "$new_processing_variables" = "luri" ]] || [[ "$new_processing_variables" = "LURI" ]] || [[ "$new_processing_variables" = "Luri" ]]; then
+			unset exitfunc
+			exitfunc="$module_set"
 		fi
 }
-function payload {
+function payload1 {
+none=" "
+myvar=""
+	function unset_value {
+		unset none
+		unset myvar
+		unset integer
+		none=" "
+		myvar=""
+	}
+lhost=`echo $lhost`
+lhost_lenght=`echo $lhost | awk '{print length}'`
+	integer=`expr 18 - $lhost_lenght`			
+	for (( i = 0 ; i < $integer; i++ )) do
+		myvar=$myvar$none
+	done
+	lhost=$lhost$myvar
+unset_value
+lport=`echo $lport`
+lport_lenght=`echo $lport | awk '{print length}'`
+	none=" "
+	myvar=""
+	integer=`expr 18 - $lport_lenght`			
+	for (( i = 0 ; i < $integer; i++ )) do
+	myvar=$myvar$none
+	done
+	lport=$lport$myvar
+unset_value
+exitfunc=`echo $exitfunc`
+exitfunc_lenght=`echo $exitfunc | awk '{print length}'`
+	integer=`expr 18 - $exitfunc_lenght`			
+	for (( i = 0 ; i < $integer; i++ )) do
+	myvar=$myvar$none
+	done
+	exitfunc=$exitfunc$myvar
+unset_value
+luri=`echo $luri`
+luri_lenght=`echo $luri | awk '{print length}'`
+	integer=`expr 18 - $luri_lenght`			
+	for (( i = 0 ; i < $integer; i++ )) do
+	myvar=$myvar$none
+	done
+	luri=$luri$myvar
+unset_value
+payload_banner_1
+}
+function payload2 {
+none=" "
+myvar=""
+	function unset_value {
+		unset none
+		unset myvar
+		unset integer
+		none=" "
+		myvar=""
+	}
+lhost=`echo $lhost`
+lhost_lenght=`echo $lhost | awk '{print length}'`
+	integer=`expr 27 - $lhost_lenght`			
+	for (( i = 0 ; i < $integer; i++ )) do
+		myvar=$myvar$none
+	done
+	lhost=$lhost$myvar
+unset_value
+lport=`echo $lport`
+lport_lenght=`echo $lport | awk '{print length}'`
+	none=" "
+	myvar=""
+	integer=`expr 27 - $lport_lenght`			
+	for (( i = 0 ; i < $integer; i++ )) do
+	myvar=$myvar$none
+	done
+	lport=$lport$myvar
+unset_value
+exitfunc=`echo $exitfunc`
+exitfunc_lenght=`echo $exitfunc | awk '{print length}'`
+	integer=`expr 27 - $exitfunc_lenght`			
+	for (( i = 0 ; i < $integer; i++ )) do
+	myvar=$myvar$none
+	done
+	exitfunc=$exitfunc$myvar
+unset_value
+luri=`echo $luri`
+luri_lenght=`echo $luri | awk '{print length}'`
+	integer=`expr 27 - $luri_lenght`			
+	for (( i = 0 ; i < $integer; i++ )) do
+	myvar=$myvar$none
+	done
+	luri=$luri$myvar
+unset_value
+payload_banner_2
+}
+function payload_banner_1 {
+if [ "$language" = "VN" ]; then
+yes="     Có   "
+no="     Không "
 cat << !
+Các tùy chọn của tải trọng (windows/x64/meterpreter/reverse_http):
 
+   Tên            Thiết lập hiện tại    Yêu cầu   Miêu tả
+   ----           ----------------      --------  -----------
+   EXITFUNC       $exitfunc$no   Biện pháp tạo lối thoát (Được chấp nhận: '', seh, thread, process, none).
+   LHOST          $lhost$yes    Địa chỉ lắng nghe.
+   LPORT          $lport$yes    Cổng lắng nghe.
+   LURI           $luri$no   Đường dẫn HTTP.
+!
+elif [ "$language" = "EN" ]; then
+yes="     yes"
+no="     no "
+cat << !
 Payload options (windows/x64/meterpreter/reverse_http):
 
-   Name      Current Setting  Required  Description
-   ----      ---------------  --------  -----------
-   EXITFUNC  $exitfunc        yes       Exit technique (Accepted: '', seh, thread, process, none)
-   LHOST     $lhost   	yes     The listen address
-   LPORT     $lport		yes     The listen port
-   LURI      $luri              no        The HTTP Path
-
+   Name           Current Setting       Required  Description
+   ----           ---------------       --------  -----------
+   EXITFUNC       $exitfunc$no      Exit technique (Accepted: '', seh, thread, process, none).
+   LHOST          $lhost$yes      The listen address.
+   LPORT          $lport$yes      The listen port.
+   LURI           $luri$no      The HTTP Path.
 
 !
+fi
 }
-function payload1 {
+function payload_banner_2 {
+if [ "$language" = "VN" ]; then
+yes="   Có  "
+no="   Không "
 cat << !
-Payload options (windows/x64/meterpreter/reverse_tcp):
+Các tùy chọn của tải trọng (windows/x64/meterpreter/reverse_http):
 
-   Name      Current Setting  		Required  Description
-   ----      ---------------  		--------  -----------
-   EXITFUNC  $exitfunc			  yes     Exit technique (Accepted: '', seh, thread, process, none)
-   LHOST     $lhost	  yes     The listen address
-   LPORT     $lport			  yes     The listen port
-   LURI      $luri			  no        The HTTP Path
+   Tên            Thiết lập hiện tại	       Yêu cầu    Miêu tả
+   ----      	  ---------------  	       --------  -----------
+   EXITFUNC       $exitfunc$no   Biện pháp tạo lối thoát (Được chấp nhận: '', seh, thread, process, none).
+   LHOST          $lhost$yes     Địa chỉ lắng nghe.
+   LPORT          $lport$yes     Cổng lắng nghe.
+   LURI           $luri$no   Đường dẫn HTTP.
 
 !
-}
+elif [ "$language" = "EN" ]; then
+yes="   yes"
+no="   no "
+cat << !
+Payload options (windows/x64/meterpreter/reverse_http):
 
+   Name           Current Setting  	       Required  Description
+   ----           ---------------  	       --------  -----------
+   EXITFUNC       $exitfunc$no      Exit technique (Accepted: '', seh, thread, process, none).
+   LHOST          $lhost$yes      The listen address.
+   LPORT          $lport$yes      The listen port.
+   LURI           $luri$no      The HTTP Path.
+
+!
+fi
+}
 function payload_run {
-path_payload=`pwd`
-rc_file="$path_payload/file.rc"
-	echo "set payload $payload_OS/$payload_format/$payload_name" >> $rc_file
-	echo "set LHOST $lhost" >> $rc_file
-	echo "set LPORT $lport" >> $rc_file
+payload_path_present=`pwd`
+payload_path_rc_file="$payload_path_present/Config"
+rc_file="$payload_path_rc_file/file.rc"
+config_file="$payload_path_present/Config/config"
+	echo "set payload windows/x64/meterpreter/reverse_http" >> $rc_file
 	echo "set EXITFUNC $exitfunc" >> $rc_file
-	echo "set LURI $luri" >> $rc_file
+	paylo="windows/x64/meterpreter/reverse_http"
+	lhost=$lhost
+	lport=$lport
 }
